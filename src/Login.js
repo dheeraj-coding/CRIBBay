74e5dc4015f723dcdb4f23793541d77dfbb74ab2
Dheeraj Mohan <dheeraj.coding@outlook.com>
2018-09-12 12:38:16 +0530

    Add user validation for email and password in sign in. Initialize firebase dev tools in this repo. 	new file:   .firebaserc 	modified:   .gitignore 	new file:   database.rules.json 	new file:   firebase.json 	new file:   firestore.indexes.json 	new file:   firestore.rules 	new file:   functions/.eslintrc.json 	new file:   functions/index.js 	new file:   functions/package-lock.json 	new file:   functions/package.json 	modified:   package-lock.json 	modified:   package.json 	new file:   public/404.html 	modified:   src/Login.js 	modified:   src/index.html 	new file:   storage.rules

--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,79 @@
 import './css/signup.scss'
-import React , {Component} from 'react';
-import { Paper, Typography, FormControl, InputLabel, Input, Button} from '@material-ui/core';
+import React, { Component } from 'react';
+import { Paper, Typography, FormControl, InputLabel, Input, Button } from '@material-ui/core';
 import { hot } from 'react-hot-loader';
 
-class Login extends Component{
-    render(){
-        return(
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+function validatePassword(pass){
+    return pass.length >=6 ;
+}
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            pass: '',
+            errorMsg: '',
+        }
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handleEmailBlur = this.handleEmailBlur.bind(this);
+        this.handlePasswordBlur = this.handlePasswordBlur.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+    handleEmailChange(event) {
+        this.setState({
+            email: event.target.value,
+        });
+    }
+    handleEmailBlur(event){
+        if(event.target.value && !validateEmail(event.target.value)){
+            this.setState({
+                errorMsg:'Enter Valid E-Mail.',
+            });
+        }else{
+            this.setState({
+                errorMsg:'',
+            })
+        }
+    }
+    handlePasswordChange(event){
+        this.setState({
+            pass:event.target.value,
+        });
+    }
+    handlePasswordBlur(event){
+        if(event.target.value && !validatePassword(event.target.value)){
+            this.setState({
+                errorMsg:'Password must be atleast 6 characters long.',
+            });
+        }else{
+            this.setState({
+                errorMsg:'',
+            });
+        }
+    }
+    render() {
+        return (
             <Paper elevation={1} className='signin'>
                 <Typography variant='headline'>Sign In</Typography>
-                <form style={{overflow:'auto'}}>
+                <Typography variant='caption' style={{ color: 'red' }}>{this.state.errorMsg}</Typography>
+                <form style={{ overflow: 'auto' }}>
                     <FormControl margin='normal' required fullWidth>
                         <InputLabel>E-Mail</InputLabel>
-                        <Input id='email' name='email' autoComplete='name' autoFocus/>
+                        <Input id='email' name='email' onChange={this.handleEmailChange} onBlur={this.handleEmailBlur}
+                            value={this.state.email} autoComplete='name' autoFocus />
                     </FormControl>
                     <FormControl margin='normal' required fullWidth>
                         <InputLabel>Password</InputLabel>
-                        <Input id='password' name='password' autoComplete='password' autoFocus/>
+                        <Input type='password' id='password' name='password' onChange={this.handlePasswordChange} onBlur={this.handlePasswordBlur} 
+                            value = {this.state.pass} autoComplete='password' autoFocus />
                     </FormControl>
                     <Button type='submit' variant='raised' fullWidth margin='normal'
-                            style={{marginTop:'1em'}}>
+                        style={{ marginTop: '1em' }}>
                         Login
                     </Button>
                 </form>
